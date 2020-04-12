f5a86b57e1cb10759668b6d4cac96325d487077a
Emma Horn Buøen <emma@Emmas-MacBook-Air.local>
2020-04-12 17:32:14 +0200

    A2: Task 3 - Completed

---
+++ TDT4265-StarterCode-master/assignment3/task3_model3d.py
@@ -0,0 +1,378 @@
+import pathlib
+import matplotlib.pyplot as plt
+import torch
+import utils
+import time
+import typing
+import collections
+from torch import nn
+from dataloaders import load_cifar10
+
+
+def compute_loss_and_accuracy(
+        dataloader: torch.utils.data.DataLoader,
+        model: torch.nn.Module,
+        loss_criterion: torch.nn.modules.loss._Loss):
+    """
+    Computes the average loss and the accuracy over the whole dataset
+    in dataloader.
+    Args:
+        dataloder: Validation/Test dataloader
+        model: torch.nn.Module
+        loss_criterion: The loss criterion, e.g: torch.nn.CrossEntropyLoss()
+    Returns:
+        [average_loss, accuracy]: both scalar.
+    """
+    average_loss = 0
+    accuracy = 0
+    total_images = 0
+    total_steps = 0
+    correct = 0
+    with torch.no_grad():
+        for (X_batch, Y_batch) in dataloader:
+            # Transfer images/labels to GPU VRAM, if possible
+            X_batch = utils.to_cuda(X_batch)
+            Y_batch = utils.to_cuda(Y_batch)
+            # Forward pass the images through our model
+            output_probs = model(X_batch)
+
+            # Compute Loss and Accuracy
+            loss = loss_criterion(output_probs, Y_batch) # tensor[value, grad_fn]
+
+            # From the example in pytorch docs:
+            _, predicted = torch.max(output_probs.data, 1)
+            # Updating variables
+            total_images += Y_batch.size(0)
+            correct += (predicted == Y_batch).sum().item() 
+            average_loss += loss.item()
+            total_steps += 1
+
+    average_loss = average_loss/total_steps
+    accuracy = correct/total_images
+    
+    return average_loss, accuracy
+
+
+class ExampleModel(nn.Module):
+
+    def __init__(self,
+                 image_channels,
+                 num_classes):
+        """
+            Is called when model is initialized.
+            Args:
+                image_channels. Number of color channels in image (3)
+                num_classes: Number of classes we want to predict (10)
+        """
+        super().__init__()
+        num_filters = 32  # Set number of filters in first conv layer
+        self.num_classes = num_classes
+        # Define the convolutional layers
+        self.feature_extractor = nn.Sequential(
+            # Layer block 1
+            nn.Conv2d(
+                in_channels=image_channels,
+                out_channels=num_filters,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ), 
+            nn.BatchNorm2d(32),
+            nn.ELU(), 
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ELU(), 
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            ),
+            # Layer block 2
+            nn.Conv2d(
+                in_channels=64, 
+                out_channels=128, 
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ), 
+            nn.BatchNorm2d(128),
+            nn.ELU(),
+            nn.Conv2d(
+                in_channels=128, 
+                out_channels=128, 
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ), 
+            nn.ELU(),
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            ),
+            # Layer block 3
+            nn.Conv2d(
+                in_channels=128, 
+                out_channels=256, 
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ), 
+            nn.BatchNorm2d(256),
+            nn.ELU(),
+            nn.Conv2d(
+                in_channels=256, 
+                out_channels=256, 
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ELU(),
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            )
+        )
+        # The output of feature_extractor will be [batch_size, num_filters, 16, 16]
+        self.num_output_features = 4096
+        # Initialize our last fully connected layer
+        # Inputs all extracted features from the convolutional layers
+        # Outputs num_classes predictions, 1 for each class.
+        # There is no need for softmax activation function, as this is
+        # included with nn.CrossEntropyLoss
+        self.classifier = nn.Sequential(
+            nn.Linear(self.num_output_features, 1024),
+            nn.ELU(),
+            nn.Linear(1024, 512),
+            nn.ELU(),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        """
+        Performs a forward pass through the model
+        Args:
+            x: Input image, shape: [batch_size, 3, 32, 32]
+        """
+        batch_size = x.shape[0]
+        out = x
+        expected_shape = (batch_size, self.num_classes)
+        # Feature layers:
+        out = self.feature_extractor(out)
+        # Flattening:
+        out = out.view(-1, self.num_output_features)
+        # Fully-Connected layers
+        out = self.classifier(out)
+        assert out.shape == (batch_size, self.num_classes),\
+            f"Expected output of forward pass to be: {expected_shape}, but got: {out.shape}"
+        return out
+
+
+class Trainer:
+
+    def __init__(self,
+                 batch_size: int,
+                 learning_rate: float,
+                 early_stop_count: int,
+                 epochs: int,
+                 model: torch.nn.Module,
+                 dataloaders: typing.List[torch.utils.data.DataLoader]):
+        """
+            Initialize our trainer class.
+        """
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.early_stop_count = early_stop_count
+        self.epochs = epochs
+
+        # Since we are doing multi-class classification, we use CrossEntropyLoss
+        self.loss_criterion = nn.CrossEntropyLoss()
+        # Initialize the model
+        self.model = model
+        # Transfer model to GPU VRAM, if possible.
+        self.model = utils.to_cuda(self.model)
+        print(self.model)
+
+        # Define our optimizer. SGD = Stochastich Gradient Descent
+        self.optimizer = torch.optim.Adam(self.model.parameters(),
+                                         self.learning_rate)
+
+        # Load our dataset
+        self.dataloader_train, self.dataloader_val, self.dataloader_test = dataloaders
+
+        # Validate our model everytime we pass through 50% of the dataset
+        self.num_steps_per_val = len(self.dataloader_train) // 2
+        self.global_step = 0
+        self.start_time = time.time()
+
+        # Tracking variables
+        self.VALIDATION_LOSS = collections.OrderedDict()
+        self.TEST_LOSS = collections.OrderedDict()
+        self.TRAIN_LOSS = collections.OrderedDict()
+        self.VALIDATION_ACC = collections.OrderedDict()
+        self.TEST_ACC = collections.OrderedDict()
+        self.TRAIN_ACC = collections.OrderedDict()
+
+        self.checkpoint_dir = pathlib.Path("checkpoints")
+
+    def validation_epoch(self):
+        """
+            Computes the loss/accuracy for all three datasets.
+            Train, validation and test.
+        """
+        # Compute for training set
+        _train_loss, train_acc = compute_loss_and_accuracy(
+            self.dataloader_train, self.model, self.loss_criterion
+        )
+        self.TRAIN_ACC[self.global_step] = train_acc
+
+        # Compute for validation set
+        self.model.eval()
+        validation_loss, validation_acc = compute_loss_and_accuracy(
+            self.dataloader_val, self.model, self.loss_criterion
+        )
+        self.VALIDATION_ACC[self.global_step] = validation_acc
+        self.VALIDATION_LOSS[self.global_step] = validation_loss
+        used_time = time.time() - self.start_time
+        print(
+            f"Epoch: {self.epoch:>2}",
+            f"Batches per seconds: {self.global_step / used_time:.2f}",
+            f"Global step: {self.global_step:>6}",
+            f"Validation Loss: {validation_loss:.2f},",
+            f"Validation Accuracy: {validation_acc:.3f}",
+            sep="\t")
+        # Compute for testing set
+        test_loss, test_acc = compute_loss_and_accuracy(
+            self.dataloader_test, self.model, self.loss_criterion
+        )
+        self.TEST_ACC[self.global_step] = test_acc
+        self.TEST_LOSS[self.global_step] = test_loss
+
+        self.model.train()
+
+    def should_early_stop(self):
+        """
+            Checks if validation loss doesn't improve over early_stop_count epochs.
+        """
+        # Check if we have more than early_stop_count elements in our validation_loss list.
+        if len(self.VALIDATION_LOSS) < self.early_stop_count:
+            return False
+        # We only care about the last [early_stop_count] losses.
+        relevant_loss = list(self.VALIDATION_LOSS.values())[-self.early_stop_count:]
+        first_loss = relevant_loss[0]
+        if first_loss == min(relevant_loss):
+            print("Early stop criteria met")
+            return True
+        return False
+
+    def train(self):
+        """
+        Trains the model for [self.epochs] epochs.
+        """
+        # Track initial loss/accuracy
+        def should_validate_model():
+            return self.global_step % self.num_steps_per_val == 0
+
+        for epoch in range(self.epochs):
+            self.epoch = epoch
+            # Perform a full pass through all the training samples
+            for X_batch, Y_batch in self.dataloader_train:
+                # X_batch is the CIFAR10 images. Shape: [batch_size, 3, 32, 32]
+                # Y_batch is the CIFAR10 image label. Shape: [batch_size]
+                # Transfer images / labels to GPU VRAM, if possible
+                X_batch = utils.to_cuda(X_batch)
+                Y_batch = utils.to_cuda(Y_batch)
+
+                # Perform the forward pass
+                predictions = self.model(X_batch)
+                # Compute the cross entropy loss for the batch
+                loss = self.loss_criterion(predictions, Y_batch)
+                self.TRAIN_LOSS[self.global_step] = loss.detach().cpu().item()
+
+                # Backpropagation
+                loss.backward()
+
+                # Gradient descent step
+                self.optimizer.step()
+
+                # Reset all computed gradients to 0
+                self.optimizer.zero_grad()
+                self.global_step += 1
+                 # Compute loss/accuracy for all three datasets.
+                if should_validate_model():
+                    self.validation_epoch()
+                    self.save_model()
+                    if self.should_early_stop():
+                        print("Early stopping.")
+                        return
+
+    def save_model(self):
+        def is_best_model():
+            """
+                Returns True if current model has the lowest validation loss
+            """
+            validation_losses = list(self.VALIDATION_LOSS.values())
+            return validation_losses[-1] == min(validation_losses)
+
+        state_dict = self.model.state_dict()
+        filepath = self.checkpoint_dir.joinpath(f"{self.global_step}.ckpt")
+
+        utils.save_checkpoint(state_dict, filepath, is_best_model())
+
+    def load_best_model(self):
+        state_dict = utils.load_best_checkpoint(self.checkpoint_dir)
+        if state_dict is None:
+            print(
+                f"Could not load best checkpoint. Did not find under: {self.checkpoint_dir}")
+            return
+        self.model.load_state_dict(state_dict)
+
+
+def create_plots(trainer: Trainer, name: str):
+    plot_path = pathlib.Path("plots")
+    plot_path.mkdir(exist_ok=True)
+    # Save plots and show them
+    plt.figure(figsize=(20, 8))
+    plt.subplot(1, 2, 1)
+    plt.title("Cross Entropy Loss")
+    utils.plot_loss(trainer.TRAIN_LOSS, label="Training loss")
+    utils.plot_loss(trainer.VALIDATION_LOSS, label="Validation loss")
+    utils.plot_loss(trainer.TEST_LOSS, label="Testing Loss")
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.title("Accuracy")
+    utils.plot_loss(trainer.VALIDATION_ACC, label="Validation Accuracy")
+    utils.plot_loss(trainer.TEST_ACC, label="Testing Accuracy")
+    plt.legend()
+    plt.savefig(plot_path.joinpath(f"{name}_plot.png"))
+    plt.show()
+
+
+if __name__ == "__main__":
+    epochs = 10
+    batch_size = 64
+    learning_rate = 0.0002
+    early_stop_count = 10
+    dataloaders = load_cifar10(batch_size)
+    model = ExampleModel(image_channels=3, num_classes=10)
+    trainer = Trainer(
+        batch_size,
+        learning_rate,
+        early_stop_count,
+        epochs,
+        model,
+        dataloaders
+    )
+    trainer.train()
+    create_plots(trainer, "task3d_model1")
+    print(
+        f"Train Accuracy: {trainer.TRAIN_ACC.popitem(last = True)}",
+        f"Test Accuracy: {trainer.TEST_ACC.popitem(last = True)},",
+        f"Validation Accuracy: {trainer.VALIDATION_ACC.popitem(last = True)}",
+        f"Train Loss: {trainer.TRAIN_LOSS.popitem(last = True)}",
+        f"Test Loss: {trainer.TEST_LOSS.popitem(last = True)},",
+        f"Validation Loss: {trainer.VALIDATION_LOSS.popitem(last = True)}",
+        sep="\t")
