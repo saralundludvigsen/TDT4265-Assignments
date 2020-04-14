f5b7bb2f886d540abd954d04290ecb3d618c12c7
Emma Horn Buøen <emma@Emmas-MacBook-Air.local>
2020-04-14 22:09:41 +0200

    A4: task 4a - try 2

--- TDT4265-StarterCode-master/assignment4/SSD/ssd/modeling/backbone/basic.py
+++ TDT4265-StarterCode-master/assignment4/SSD/ssd/modeling/backbone/basic.py
@@ -175,8 +175,12 @@ class BasicModel(torch.nn.Module):
         where out_features[0] should have the shape:
             shape(-1, output_channels[0], 38, 38),
         """
-        out_features = []
+        out_features = [self.bank1, self.bank2, self.bank3, self.bank4, self.bank5, self.bank6]
         for idx, feature in enumerate(out_features):
+            out_channel = self.output_channels[idx]
+            x = feature(x)
+            feature_map_size = x.shape[0]
+            out_features.append(x)
             expected_shape = (out_channel, feature_map_size, feature_map_size)
             assert feature.shape[1:] == expected_shape, \
                 f"Expected shape: {expected_shape}, got: {feature.shape[1:]} at output IDX: {idx}"
